76a8d7ea103964e7c7fd1e322929db670e3a2640
Yamil Tactuk <yamil.tactuk@gmail.com>
2024-05-02 22:12:53 -0600

    Changed staticwebassets to a relative path instead of an absolute one. Try 5

--- HomePage/obj/Debug/net8.0/HomePage.AssemblyInfo.cs
+++ HomePage/obj/Debug/net8.0/HomePage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomePage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126f41638af433a88709ada411f4e186b6394274")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd701cac9ad31f8d8a8832b83295c19dc4ffb01e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomePage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomePage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
